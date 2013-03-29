df924d6a3b7c190fa0541c5c6b79bf1e9e87cfe0
Shani Elharrar <atnt123@bezeqint.net>
2013-03-29 20:13:11 +0300

    Initial script check-in

---
+++ scripts/jquery.view-animations.js
@@ -0,0 +1,81 @@
+(function($) {
+
+    /**
+    * Copyright 2012, Digital Fusion
+    * Licensed under the MIT license.
+    * http://teamdf.com/jquery-plugins/license/
+    *
+    * @author Sam Sehnert
+    * @desc A small plugin that checks whether elements are within
+    *     the user visible viewport of a web browser.
+    *     only accounts for vertical position, not horizontal.
+    */
+
+    var win               = $(window);
+
+    $.fn.visible = function() {
+
+        var target            = $(this),
+  
+        // On the both next sections : bottom is larger than top!
+
+        // Defines the range of Y that is shown on screen
+        viewTop           = win.scrollTop(),
+        viewBottom        = viewTop + win.height(),
+  
+        // Defines the range of Y of the given object
+        targetTop         = target.offset().top,
+        targetBottom      = _top + target.height(),
+
+        // Whether on screen or not
+        isOnView          = targetBottom <= viewBottom && targetTop >= viewTop;
+  
+
+        return isOnView;
+
+    };
+
+    /**
+    * @author Shani Elharrar
+    * @desc When called, calls the callback when 
+    * the element is visible for the first time.
+    */
+    $.fn.onVisibile = function(callback) {
+
+        var target = $(this);
+
+        var scrollCallback = function() {
+            
+            if (target.visible()) {
+                
+                win.unbind('scroll', scrollCallback); 
+                callback(target);   
+            }
+        };
+
+        win.scroll(scrollCallback);
+
+    }
+    
+    /**
+    * @author Shani Elharrar
+    * @desc Attaches 'onVisible' for elements
+    * that have data-on-visible-class 
+    * and adds the wanted class on the callback
+    */
+    
+    $('*[data-on-visible-class]').each(function(index, item) {
+        
+        var target = $(item);
+        
+        var classToAdd = target.data('on-visible-class');
+        
+        var callback = function() {
+            target.add(classToAdd);
+        };
+        
+        target.onVisible(callback);
+        
+    })
+
+})(jQuery);
